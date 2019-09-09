73cc082ce20c1812746723c6f94b4e3c8b57e3ae
talesmm14 <talesmelquiades@hotmail.com>
2019-09-09 20:03:59 -0300

    Operadores de Atribuição

---
+++ fundamentos/atribuicao/atribuicao.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var v byte = 3
+	fmt.Println(v)
+
+	i := 3 // inferência de tipo
+	i += 3 // i = i + 3
+	i -= 3 // i = i - 3
+	i /= 2 // i = i / 2
+	i *= 2 // i = i * 2
+	i %= 2 // i = i % 2
+
+	fmt.Println(i)
+
+	x, y := 1, 2
+	fmt.Println(x, y)
+
+	x, y = y, x
+	fmt.Println(x, y)
+}
